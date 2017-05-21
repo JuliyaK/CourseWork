073d6a125a9039d8b542e80e26e04871e4897773
JuliyaK <y.kashlakova@yandex.ru>
2017-05-21 12:45:04 +0700

    Шахматная доска

--- Chessboard.cs
+++ Chessboard.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace CourseWork
 {
-    class Chessboard
+    class Chessboard:Panel
     {
         string сolorCell = null;
         public Cell [] arrayCells;
@@ -29,11 +31,11 @@ namespace CourseWork
                 arrayCells[i] = new Cell(сolorCell);
                 if (i % 2 == 0)
                 {
-                    arrayCells[i].Color = "White";
+                    arrayCells[i].FigureColor = "White";
                 }
                 else
                 {
-                    arrayCells[i].Color = "Blak";
+                    arrayCells[i].FigureColor = "Blak";
                 }
                 if (i % 8 == 0)
                 {
@@ -95,7 +97,37 @@ namespace CourseWork
                 }
                 figure.CurrentCell = arrayCells[i];
                 arrayCells[i].CurrentFigure = figure;
+                arrayCells[i].Controls.Add(figure);
             }
+        }
+        public void CreateChessPanel()
+        {
+            Panel chessPanel = new Panel();
+            PictureBox pictureChess = new PictureBox();
+            //TextBox textBox1 = new TextBox();
+           // Label label1 = new Label();
+
+            // Initialize the Panel control.
+            chessPanel.Location = new Point(56, 72);
+            chessPanel.Size = new Size(264, 152);
+            // Set the Borderstyle for the Panel to three-dimensional.
+            chessPanel.BorderStyle = BorderStyle.Fixed3D;
+
+            // Initialize the Label and TextBox controls.
+            pictureChess.Location = new Point(16, 16);
+            pictureChess.Size = new Size(64, 64);
+           // label1.Location = new Point(16, 16);
+           // label1.Text = "label1";
+          //  label1.Size = new Size(104, 16);
+          //  textBox1.Location = new Point(16, 32);
+           // textBox1.Text = "";
+          //  textBox1.Size = new Size(152, 20);
+
+            // Add the Panel control to the form.
+            this.Controls.Add(chessPanel);
+            // Add the Label and TextBox controls to the Panel.
+            chessPanel.Controls.Add(pictureChess);
+          //  panel1.Controls.Add(textBox1);
         }
     }
 }
